baa48c5ed07290b1142e1b744618ac076117ee81
Willi24am <80512313+Willi24am@users.noreply.github.com>
2021-03-12 23:07:20 +0700

    Tugas 2 Praktek Programa Komputer

---
+++ I0320110_Soal1_Tugas2.py
@@ -0,0 +1,51 @@
+#menghitung luas persegi panjang
+print("luas persegi panjang")
+
+p = 20
+l = 5
+
+luas_persegi_panjang = p * l
+
+print("panjang: ", p)
+print("luas: ", l)
+print("luas persegi panjang: ",  luas_persegi_panjang)
+
+#menghitung luas lingkaran
+print("luas lingkaran")
+
+r = 14
+
+luas_lingkaran = 3.14 * (r ** 2)
+
+print("jari-jari: ", r)
+print("luas lingkaran: ", luas_lingkaran)
+
+#menghitung luas kubus
+print("luas kubus")
+
+s = 8
+
+luas_kubus = 6 * (s ** 2)
+
+print("sisi: ", s)
+print("luas kubus: ", luas_kubus)
+
+#menghitung konversi suhu celcius ke fahrenheit
+print("konversi suhu (celcius ke fahrenheit)")
+
+C = 40
+
+F = C * (9/5) + 32
+
+print("celcius: ", C)
+print("fahrenheit: ", F)
+
+#menghitung konversi suhu reamur ke kelvin
+print("konversi suhu (reamur ke kelvin)")
+
+R = 20
+
+K = (5/4) * R + 273
+
+print("reamur: ", R)
+print("kelvin: ", K)
---
+++ I0320110_Soal2_Tugas2.py
@@ -0,0 +1,34 @@
+nama = "William Anderson"
+nama_panggilan = "Willi"
+tanggal_lahir = 19
+bulan_lahir = "Juli"
+tahun_lahir = 2002
+umur = 18
+tinggi_badan = 165.5
+berat_badan = 53.7
+agama = "Buddha"
+alamat = "Jl.Budi mulia no 33 RT/RW 003/04 Jakarta Utara"
+golongan_darah = "O"
+anak_ke = 3
+saudara_kandung = 3
+sma = "SMAN 40 Jakarta"
+jurusan = "Teknik Industri"
+kuliah = "Universitas Sebelas Maret"
+hobi = "badminton"
+makanan_favorit = "telur goreng"
+minuman_favorit = "susu"
+
+print("Biodata")
+print("Nama saya ", nama, "saya biasa dipanggil ", nama_panggilan)
+print("Saya lahir pada tanggal ", tanggal_lahir, "bulan ", bulan_lahir, "tahun ", tahun_lahir)
+print("Sekarang saya berumur ", umur, "tahun")
+print("Saya beragama ", agama)
+print("Tinggi saya ", tinggi_badan, "cm ", "berat badan saya ", berat_badan, "kg")
+print("Hobi saya ", hobi)
+print("Golongan darah saya ", golongan_darah)
+print("Saya tinggal di ", alamat)
+print("Saya merupakan anak ke ", anak_ke, "dari ", saudara_kandung, "bersaudara")
+print("Saya dulu bersekolah di ", sma)
+print("Sekarang saya berkuliah di ", jurusan, kuliah)
+print("Saya suka makan ", makanan_favorit)
+print("Minuman kesukaan saya adalah ", minuman_favorit)
